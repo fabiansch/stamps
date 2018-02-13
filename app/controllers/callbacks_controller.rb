3f28213764263d2e9a7d9d008cd6811b5118b290
fabian <stahlschiff@web.de>
2018-02-13 14:48:25 -0600

    try bugfix 2

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,13 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    def twitter
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
+  def twitter
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      # http://stackoverflow.com/questions/7117200/devise-for-twitter-cookie-overflow-error
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except('extra')
+      redirect_to new_user_registration_url
     end
+  end
 end
