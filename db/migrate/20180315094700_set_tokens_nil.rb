bf7fb7d856d25e87d2aba301f884423eb0ca2df8
fabian <stahlschiff@web.de>
2018-03-15 10:50:53 +0100

    add migration that nils all tokens

---
+++ db/migrate/20180315094700_set_tokens_nil.rb
@@ -0,0 +1,15 @@
+class SetTokensNil < ActiveRecord::Migration[5.1]
+  def change
+
+    reversible do |direction|
+      direction.up do
+        User.find_each do |user|
+          user.uid = user.email
+          user.tokens = nil
+          user.save!
+        end
+      end
+    end
+
+  end
+end
