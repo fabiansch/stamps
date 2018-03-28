39782f78e9c2695f409b747fd2ca5412d317af10
fabian <stahlschiff@web.de>
2018-03-28 12:17:05 +0200

    api return cards ordered by newest stamp

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,8 +2,15 @@ class Api::PagesController < Api::ApplicationController
   def home
       cards = []
       current_user.cards.each do |card|
-        cards.insert(-1, {card: card, current_stamps: card.stamps.where(user: current_user).count})
+        stamps = card.stamps.where(user: current_user)
+        cards.insert(-1, {card: card,
+                          current_stamps: stamps.count,
+                          newest_stamp: stamps.order(:created_at).last.created_at})
+
       end
+      cards = cards.sort_by{|c| c[:newest_stamp]}
+      cards.reverse!
+
       respond_with cards
   end
 end
